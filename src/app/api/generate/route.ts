2685959cc564744d73ef86345b8ecc0fa835b098
Raj Varsani <varasaniraj5@gmail.com>
2024-06-09 22:21:47 +0530

    feat: Integrate `kv` store

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,7 +1,7 @@
 import { InternetSource } from "@/app/types/sources";
 import { queryAssistantV1 } from "@/services/ai";
 import { getTopicSources } from "@/services/search";
-import { getInitializedUser } from "@/services/users";
+import { getUserWithInitialization, updateUserCredits } from "@/services/users";
 import { OPENAI_ASSISTANTS } from "@/utils/constants";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -15,17 +15,17 @@ type CarouselItem = {
 export async function GET(req: NextRequest) {
   try {
     // TODO: get user's address from headers
-    // const userAddress = "0x1234567890abcdef1234567890abcdef12345678";
-    // const userData = await getInitializedUser(userAddress);
-    // if (userData.credits < 1) {
-    //   return NextResponse.json(
-    //     {
-    //       error:
-    //         "Insufficient credits. Please purchase more credits to continue.",
-    //     },
-    //     { status: 402 }
-    //   );
-    // }
+    const userAddress = "0x1234567890abcdef1234567890abcdef12345678";
+    const userData = await getUserWithInitialization(userAddress);
+    if (userData.credits < 1) {
+      return NextResponse.json(
+        {
+          error:
+            "Insufficient credits. Please purchase more credits using BONK tokens.",
+        },
+        { status: 402 }
+      );
+    }
     const topic = req.nextUrl.searchParams.get("topic");
     if (!topic) {
       return NextResponse.json(
@@ -52,12 +52,13 @@ export async function GET(req: NextRequest) {
       throw e;
     }
 
+    await updateUserCredits(userAddress, 1);
     return NextResponse.json(carouselData, { status: 200 });
-  } catch (err) {
+  } catch (err: any) {
     console.log(err);
     return NextResponse.json(
       {
-        error: "Internal server error.",
+        error: err.message || "An error occurred while processing the request.",
       },
       { status: 500 }
     );
--- src/services/users.ts
+++ src/services/users.ts
@@ -1,25 +1,43 @@
-import kv from "@vercel/kv";
+import { createClient } from "@vercel/kv";
 
 const DEFAULT_CREDITS = 3;
 
-const getNewUser = async () => {
-  return {
-    credits: DEFAULT_CREDITS,
-    createdAt: Date.now(),
-  };
-};
+const kvClient = createClient({
+  url: process.env.KV_REST_API_URL as string, // Ensure type is string
+  token: process.env.KV_REST_API_TOKEN as string,
+  cache: "no-cache",
+});
 
-export const getInitializedUser = async (userAddress: string) => {
-  const userData = (await kv.get(userAddress)) as {
-    credits: number;
-    createdAt: number;
-  };
+const generateUserKey = (userAddress: string) => `user:${userAddress}`;
+const createDefaultUserData = () => ({
+  credits: DEFAULT_CREDITS,
+  createdAt: Date.now(),
+});
 
-  if (!userData) {
-    const user = await getNewUser();
-    await kv.set(userAddress, user);
-    return user;
+export const getUserWithInitialization = async (userAddress: string) => {
+  const userKey = generateUserKey(userAddress);
+  const user = await kvClient.get<{ credits: number; createdAt: number }>(
+    userKey
+  );
+  console.log("RETRIEVED USER: ", userKey, user);
+
+  if (!user) {
+    const userData = createDefaultUserData();
+    console.log("CREATED USER: ", userKey, userData);
+    await kvClient.set(userKey, userData);
+    return userData;
   }
 
-  return userData;
+  return user;
+};
+
+export const updateUserCredits = async (
+  userAddress: string,
+  usageCredits: number
+) => {
+  const userData = await getUserWithInitialization(userAddress);
+  userData.credits -= usageCredits;
+
+  console.log("UPDATED USER: ", userData);
+  await kvClient.set(generateUserKey(userAddress), userData);
 };
