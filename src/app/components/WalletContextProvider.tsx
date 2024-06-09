e9f6f1e9bec76d3e3a50cdbdb848abb30aeb4d23
Raj Varsani <varasaniraj5@gmail.com>
2024-06-09 21:10:14 +0530

    fix: build ts issue

--- src/app/components/WalletContextProvider.tsx
+++ src/app/components/WalletContextProvider.tsx
@@ -1,25 +1,17 @@
 // WalletContextProvider.tsx
 "use client";
-import React, { FC, useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
 
-const WalletContextProvider: FC = ({
-  children,
-}: {
-  children?: React.ReactNode;
-}) => {
+const WalletContextProvider = ({ children }: { children?: any }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], [network]);
