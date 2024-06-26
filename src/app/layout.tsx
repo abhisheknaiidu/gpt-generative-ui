db732c6a00d0f56b76d60a740ce8401596a21e29
Raj Varsani <varasaniraj5@gmail.com>
2024-06-27 00:24:22 +0530

    refactor: Update favicon link in layout.tsx
    Update the favicon link in layout.tsx to use the "/favicon.svg" file instead of "/favicon.ico". This change ensures that the correct favicon is displayed in the browser tab and improves the visual consistency of the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#FDDE00" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta
           name="apple-mobile-web-app-title"
